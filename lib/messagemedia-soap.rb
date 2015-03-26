ed01e75ea6da59e01eee38ec0cd487523e15bf41
Tristan Penman <tristan@tristanpenman.com>
2015-03-26 12:46:11 +1100

    Use single quotes in main module file

--- lib/messagemedia-soap.rb
+++ lib/messagemedia-soap.rb
@@ -1,4 +1,4 @@
-require "messagemedia/soap/version"
-require "messagemedia/soap/recipient"
-require "messagemedia/soap/message"
-require "messagemedia/soap/client"
+require 'messagemedia/soap/version'
+require 'messagemedia/soap/recipient'
+require 'messagemedia/soap/message'
+require 'messagemedia/soap/client'
