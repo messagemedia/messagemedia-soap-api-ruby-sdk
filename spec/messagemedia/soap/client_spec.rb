76c138e28807eeb6464288c70fddc92675e6debb
Tristan Penman <tristan@tristanpenman.com>
2015-03-31 14:34:36 +1100

    Add test case for get_user_info method in Client class

--- spec/messagemedia/soap/client_spec.rb
+++ spec/messagemedia/soap/client_spec.rb
@@ -198,4 +198,63 @@ describe Messagemedia::SOAP::Client do
     end
   end
 
+  describe '#get_user_info' do
+    it 'returns user info' do
+
+      expected_request_body = {
+          :'api:authentication' => {
+              :'api:userId' => 'username',
+              :'api:password' => 'password'
+          }
+      }
+
+      packaged_response = {
+          :code => 200,
+          :headers => {},
+          :body => Gyoku.xml({
+              :'SOAP-ENV:Envelope' => {
+                  :'@xmlns:SOAP-ENV' => 'http://schemas.xmlsoap.org/soap/envelope/',
+                  :'@xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema',
+                  :'@xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+                  :'@xmlns:SOAP-ENC' => 'http://schemas.xmlsoap.org/soap/encoding/',
+                  :'SOAP-ENV:Body' => {
+                      :check_user_response => {
+                          :@xmlns => 'http://xml.m4u.com.au/2009',
+                          :result => {
+                              :account_details => {
+                                  :@type => 'daily',
+                                  :@credit_limit => '500',
+                                  :@credit_remaining => '497'
+                              },
+                              :@sent => '2',
+                              :@scheduled => '0',
+                              :@failed => '0'
+                          }
+                      }
+                  }
+              }
+          },
+          {
+              :key_converter => :none
+          })
+      }
+
+      savon.expects(:check_user).with(message: expected_request_body).returns(packaged_response)
+
+      client = described_class.new('username', 'password')
+
+      result = client.get_user_info
+
+      expect(result).to_not be_nil
+      expect(result[:account_details]).to_not be_nil
+      expect(result[:account_details][:@type]).to eq('daily')
+      expect(result[:account_details][:@credit_limit]).to eq('500')
+      expect(result[:account_details][:@credit_remaining]).to eq('497')
+      expect(result[:@sent]).to eq('2')
+      expect(result[:@scheduled]).to eq('0')
+      expect(result[:@failed]).to eq('0')
+
+    end
+  end
+
 end
